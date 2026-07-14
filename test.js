a31332ea32704394df9527687e7baadf502a945f
Ayush Kumar Sinha <ayushh.dev05@gmail.com>
2026-07-14 12:06:41 +0530

    feat:add CI Pipeline

---
+++ test.js
@@ -0,0 +1,9 @@
+console.log("this file name is test.js");
+
+console.log("test started.....");
+
+setTimeout(() => {
+  console.log("waiting 3 seconds....");
+}, 3000);
+
+console.log("test completed");
